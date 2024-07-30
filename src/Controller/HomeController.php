7942c3956f86019f71914c10b0d4ac90de897f67
LeoVanscoor <vanscoorleo@gmail.com>
2024-07-30 15:26:53 +0200

    creation of the user profile page, and also the blog list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BlogPostRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(BlogPostRepository $blogPostRepository): Response
     {
+        $blogposts = $blogPostRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'blogposts' => $blogposts,
         ]);
     }
 }
